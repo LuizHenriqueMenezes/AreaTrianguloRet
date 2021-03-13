f203773a8ad845ee2e87aeec13457944633834ec
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-13 15:19:09 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos calcular a área de um triângulo retângulo, para isso digite o valor da... ");
+           
+            Console.WriteLine("Base: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Altura: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            double divisao = 2;
+
+            Console.WriteLine("O resultado do calculo da área é: "+ b * h / divisao);
+             
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AreaTrianguloRet
+
 Calcule a área de um triângulo retângulo, dados base (b) e altura (h). A área é dada pela metade do produto da base pela altura.
+
+```cs
+Base..: 3
+Altura: 5
+
+Área..: 7.5
+```
